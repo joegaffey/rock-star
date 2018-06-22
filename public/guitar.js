3d64452eaad53f57106ca643ab2793a31eb5a0d4
Glitch (rock-star) <none>
2018-06-22 21:17:42 +0000

    Added h-bar and fixed some graphics timing issues

--- public/guitar.js
+++ public/guitar.js
@@ -21,6 +21,7 @@ guitarTemplate.innerHTML = `
     <line y2="400" x2="99" y1="0" x1="99" stroke-width="3" stroke="#bbb" stroke-dasharray="4 0.5" />
     <line y2="400" x2="132" y1="0" x1="132" stroke-width="2.5" stroke="#bbb" stroke-dasharray="4 0.5" />
     <line y2="400" x2="165" y1="0" x1="165" stroke-width="2" stroke="#bbb" />
+    <line y2="325" x2="200" y1="325" x1="0" stroke-width="2" stroke="#bbb" />
   </g>
 </svg>
 `;
@@ -38,6 +39,7 @@ export default class Guitar extends Instrument {
     this.windowSize = 2; // Time window in seconds
     this.scale = 50; // Used map note time to graphics - to be tweaked
     this.mNoteIndex = 0; // Current position in track
+    this.offset = 75;
     
     this.noteToStringMap = { A:0, B:1, C:2, D:2, E:3, F:3, G:4 };   
   }
@@ -62,14 +64,15 @@ export default class Guitar extends Instrument {
     this.gNotes.forEach((note, i) => {
       // Move notes
       let y = (now - note.mNote.time) * this.scale;// * note.mNote.velocity;
-      note.setAttribute('y1', note._y1 = y);
-      note.setAttribute('y2', note._y2 = y - note.length);
-      note.gNoteCentre.setAttribute('cy', y);
+      note.setAttribute('y1', note._y1 = y + this.offset);
+      note.setAttribute('y2', note._y2 = y + this.offset - note.length);
+      note.gNoteCentre.setAttribute('cy', y + this.offset);
       
       // Remove notes outside the render area
-      if(note._y1 > 420) {
+      if(note._y1 - note.length > 420) {
         this.gNotes.splice(i, 1);
         this.graphics.removeChild(note);
+        this.graphics.removeChild(note.gNoteCentre);
       }
     });
     
@@ -80,7 +83,7 @@ export default class Guitar extends Instrument {
       if(mNote.time > now && mNote.time < now + this.windowSize && mNote.duration > 0) {
         let string = this.noteToStringMap[mNote.name.substring(0,1)]; // First charter of note only
         if(mNote.time > 0 && !mNote.added) {
-          this.addNote(string * 33 + 33, -1000, mNote.duration * this.scale / 2, this.colors[string], mNote);
+          this.addNote(string * 33 + 33, -1000, mNote.duration * this.scale, this.colors[string], mNote);
           mNote.added = true; // Only add notes once
         }
         this.mNoteIndex = i;
@@ -99,7 +102,7 @@ export default class Guitar extends Instrument {
     gNote.setAttribute('stroke', color);
     gNote.setAttribute('stroke-linecap', 'round');
     gNote.setAttribute('stroke-width', '30');
-    gNote.setAttribute('opacity', 0.2);
+    gNote.setAttribute('opacity', 0.1);
     // gNote.setAttribute('filter', 'url(#glow)');
     gNote._y1 = y;
     gNote._y2 = y + length;
