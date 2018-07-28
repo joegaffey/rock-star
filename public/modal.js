4d77c7522a6a7d1f47c380a4aa31d89fd571fb1e
Glitch (rock-star) <none>
2018-07-28 12:15:17 +0000

    Added modal class and some progress on controls config

---
+++ public/modal.js
@@ -0,0 +1,30 @@
+export default class Modal {
+  
+  constructor(element) {
+    this.element = element;
+    this.onScreen = false;
+    const closeEl = document.createElement('span');
+    closeEl.innerHTML = 'X';    
+    closeEl.classList.add('closeModal');
+    this.element.querySelector('.modalTitle').appendChild(closeEl);
+    closeEl.onclick = () => {
+      this.toggle();
+    }
+  }
+  
+  toggle() {
+    if(this.onScreen) {
+      this.onClose();
+      this.element.style.display = 'none';
+      this.onScreen = false;
+    }
+    else {
+      this.onOpen();
+      this.element.style.display = 'flex';
+      this.onScreen = true;
+    }
+  }
+  
+  onOpen() {};    
+  onClose() {};    
+}
